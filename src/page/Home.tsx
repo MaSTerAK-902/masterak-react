2de21aa80366b7983da30a5d49e4b3a635c5dc2c
masterak <htakuma@masterak-arch.com>
2022-08-15 10:46:46 +0900

    # テキストスライダー

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 
 const message_array = ['Hi! My name is Masterak.', 'I am Engineer.', 'ゆっくりしていってね！']
-let counter = 0
-
-const countUp = () => {
-	console.log(message_array[counter])
-	counter = counter +1
-	if(counter == 2){
-		counter = 0
-	}
-}
-
-setInterval(countUp, 5000)
 
 const home = () => {
+	const [counter, setCount] = useState(0)
+	const useCountUp = () => {
+		setCount(counter + 1)
+		if (counter == 2) {
+			setCount(0)
+		}
+	}
+	setInterval(useCountUp, 5000)
 	return (
 		<div className="container">
 			<div className="comment">
