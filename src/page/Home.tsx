fbdc55870d143b56dcd4d7ebabb611b7d41413a3
masterak <htakuma@masterak-arch.com>
2022-08-16 11:07:25 +0900

    # Contact Page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.scss'
+import { Link } from 'react-router-dom'
 
 const home = () => {
 	const linkClick_Github = () => {
@@ -9,7 +10,7 @@ const home = () => {
 		window.open('https://twitter.com/MaSTerAK_O2O9', '_blank')
 	}
 	const linkClick_Zenn = () => {
-		window.open('https://zenn.dev/masterak', '_blank')
+		window.open('https://zenn.dev/masterak')
 	}
 	const linkClick_Contact = () => {
 		window.open('/contact', '_blank')
@@ -25,7 +26,7 @@ const home = () => {
 				<div className="box github_link" onClick={linkClick_Github} />
 				<div className="box twitter_link" onClick={linkClick_Twitter} />
 				<div className="box zenn_link" onClick={linkClick_Zenn} />
-				<div className="box contact_link" onClick={linkClick_Contact} />
+				<Link to="/contact"><div className="box contact_link"/></Link>		
 			</div>
 		</div>
 	)
--- src/page/contact.tsx
+++ src/page/contact.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './style.scss'
+
 
 const contact = () => {
-	return <div>ContactPage</div>
+	return (
+		<div className="container">
+			<p>Your Name</p>
+			<input />
+			<p>Your E-mail</p>
+			<input />
+			<p>Your Message</p>
+			<input />
+			<button>send</button>
+		</div>
+	)
 }
 
 export default contact
